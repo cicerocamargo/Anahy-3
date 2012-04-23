b49a9da5049ac8fd4940f1cf1a19cffcf305c482
Cícero Camargo <camargo.cicero@gmail.com>
2012-04-23 15:38:24 -0300

    mais definicoes...

--- Anahy/definitions.h
+++ Anahy/definitions.h
@@ -7,3 +7,4 @@ typedef unsigned int                   uint;
 typedef unsigned long int              ulong;
 typedef void*(*pfunc)(void*); // signature of a parallel funtion
 typedef Job*(*sfunc)(list<Job*>, Job*); // signature of a scheduling funtion
+typedef void* athread_attr_t; // PROVISORIO!!!
